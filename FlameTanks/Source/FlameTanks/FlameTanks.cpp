e4ed9be5913e6d2211106fe83cc1f4d40b331cf5
shawn.huang <shawncc.huang@gmail.com>
2018-02-09 16:27:39 +0800

    Init project , Add Lanscape

---
+++ FlameTanks/Source/FlameTanks/FlameTanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FlameTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlameTanks, "FlameTanks" );
---
+++ FlameTanks/Source/FlameTanks/FlameTanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlameTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAMETANKS_API AFlameTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
